bba1037ca613da353f1b7ff2ac3c215d9a56974c
Leks12lk <alexa12lk@yandex.ru>
2017-04-24 15:15:54 +0300

    added change navbar with ajax

--- scripts/controllers/mainCtrl.js
+++ scripts/controllers/mainCtrl.js
@@ -3,19 +3,31 @@
         changeNavbar: function () {
             var isAuthorized = config.isAuthorized;
 
-            var anonElement =
-				'<li><a href="javascript: routeProvider.getPage(\'register\');"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>';
-            anonElement +=
-				'<li><a href="javascript: routeProvider.getPage(\'login\');"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>';
+            //var anonElement =
+			//	'<li><a href="javascript: routeProvider.getPage(\'register\');"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>';
+            //anonElement +=
+			//	'<li><a href="javascript: routeProvider.getPage(\'login\');"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>';
 
-            var authElement =
-				'<li><a href="#"> ' + config.userName + '</a></li>';
-            authElement +=
-				'<li><a href="javascript: authCtrl.logout();"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>';
+            //var authElement =
+			//	'<li><a href="#"> ' + config.userName + '</a></li>';
+            //authElement +=
+			//	'<li><a href="javascript: authCtrl.logout();"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>';
 
-            var element = isAuthorized ? authElement : anonElement;
+            //var element = isAuthorized ? authElement : anonElement;
 
-            $(".change-nav").html(element);
+        	//$(".change-nav").html(element);
+	        var _url = isAuthorized ? '/views/navbar/auth.navbar.html' : '/views/navbar/anon.navbar.html';
+	        $.ajax({
+	        	url: _url,
+	        	type: 'GET',
+				success: function(data) {
+					$('.navbar').html(data);
+					$('.username').text(config.userName);
+				},
+				error: function(data) {
+					console.log('changeNavbar error', data);
+				}
+	        });
         },
 
         clearConfig: function () {
