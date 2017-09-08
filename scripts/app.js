82809eceff6cdc00b0f0f12556dbf87955d1f647
Aleksey Riabyshev <alexa12lk@yandex.ru>
2017-09-08 03:34:49 -0700

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -5,10 +5,10 @@ config.userName = localStorage.getItem('userName');
 config.headers = {
 	'Authorization': 'Bearer ' + config.token
 }
-config.baseUrl = 'http://learn-todo.gear.host/api/tasks';
-config.registerUrl = 'http://learn-todo.gear.host/api/account/register';
-config.tokenUrl = 'http://learn-todo.gear.host/token';
-config.logoutUrl = 'http://learn-todo.gear.host/api/account/logout';
+config.baseUrl = '//learn-todo.gear.host/api/tasks';
+config.registerUrl = '//learn-todo.gear.host/api/account/register';
+config.tokenUrl = '//learn-todo.gear.host/token';
+config.logoutUrl = '//learn-todo.gear.host/api/account/logout';
 config.isAuthorized = false;
 config.rootFolderName = 'todo-app';
 
